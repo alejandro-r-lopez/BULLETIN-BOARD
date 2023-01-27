857bfae73e38680ad01083300e54ef435c2c344f
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-26 21:11:05 -0500

    began styling

--- render.utils.js
+++ render.utils.js
@@ -1,12 +1,14 @@
 export function renderPost(post) {
     const postBox = document.createElement('div');
-    const postTitle = document.createElement('h2');
+    const postTitle = document.createElement('p');
     const postMessage = document.createElement('p');
     const postAuthor = document.createElement('p');
 
-    postTitle.textContent = post.title;
+    postBox.classList.add('postbox-style');
+
+    postTitle.textContent = `${post.title}:`;
     postMessage.textContent = post.message;
-    postAuthor.textContent = post.author;
+    postAuthor.textContent = `---${post.author}`;
 
     postBox.append(postTitle, postMessage, postAuthor);
 
