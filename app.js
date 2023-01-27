7ac1f118876ebe983840554ebbd0f6d3c1121fae
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-26 19:02:00 -0500

    displaying list of posts

--- app.js
+++ app.js
@@ -12,10 +12,27 @@
 
 import { getPosts } from './fetch-utils.js';
 
+const postsSection = document.querySelector('.posts-section');
+
 let postsData = [];
 
 window.addEventListener('load', async () => {
     const gotPosts = await getPosts();
     postsData = gotPosts;
-    console.log(postsData);
+
+    for (let post of postsData) {
+        const createdPost = renderPost(post);
+        postsSection.append(createdPost);
+    }
 });
+
+function renderPost(post) {
+    const postBox = document.createElement('div');
+    const postTitle = document.createElement('h2');
+    const postMessage = document.createElement('p');
+    const postAuthor = document.createElement('p');
+
+    postTitle.textContent = post.title;
+    postBox.append(postTitle);
+    return postBox;
+}
